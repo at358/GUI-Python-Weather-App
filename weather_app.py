496ba8f53d8b908168e0e9be1eda0d0a3a0150f2
at358 <at358@njit.edu>
2021-05-27 10:25:00 -0400

    Creating Basic GUI Application

---
+++ weather_app.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import requests
+from PIL import Image, ImageTk
+
+app = tk.Tk()
+
+HEIGHT = 500
+WIDTH = 600
+
+def format_response(weather_json):
+    try:
+        city = weather_json['name']
+        conditions = weather_json['weather'][0]['description']
+        temp = weather_json['main']['temp']
+        final_str = 'City: %s \nConditions: %s \nTemperature (°F): %s' % (city, conditions, temp)
+    except:
+        final_str = 'There was a problem retrieving that information'
+    #final_str = 'hello'
+    return final_str
+
+
+app.mainloop()
