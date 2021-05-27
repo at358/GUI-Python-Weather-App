b509f0be7153495431d76919080a890b2a02cbba
at358 <at358@njit.edu>
2021-05-27 10:27:25 -0400

    Create Search button
    Enter city and will return weather data from that city at that moment and will return error if an invalid city or improper formating is entered

---
+++ get_city_weather_app.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+import requests
+from PIL import Image, ImageTk
+
+app = tk.Tk()
+
+HEIGHT = 500
+WIDTH = 600
+
+def format_response(weather_json):
+    try:
+        city = weather_json['name']
+        conditions = weather_json['weather'][0]['description']
+        temp = weather_json['main']['temp']
+        final_str = 'City: %s \nConditions: %s \nTemperature (°F): %s' % (city, conditions, temp)
+    except:
+        final_str = 'There was a problem retrieving that information'
+    #final_str = 'hello'
+    return final_str
+
+def get_weather(city):
+    weather_key = 'edffd1bf975a74d5d10e58c5ac8be2d3'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID': 'edffd1bf975a74d5d10e58c5ac8be2d3', 'q': city, 'units':'imperial'}
+    response = requests.get(url, params=params)
+    print(response.json())
+    weather_json = response.json()
+
+    results['text'] = format_response(response.json())
+
+    icon_name = weather_json['weather'][0]['icon']
+    open_image(icon_name)
+
+
+
+app.mainloop()
